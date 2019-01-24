109ace9852eac9e5927808a61b64eae96d33e313
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2019-01-23 21:54:43 -0800

    updated README

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Renting The House
 This is my breakdown of the potential benefits from renting out the house. Note
 that this is after accounting for a 600/month rent subsidy for mom.
+![](monthly_cash_flow.png)
+
+![](yearly_cash_flow.png)
 
 ![](cum_cash_flow.png)
 
--- mom_financial_projection.py
+++ mom_financial_projection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
 # monthly income
 scenarios = [(0,0,4,0,0), (0,0,4,20,.25), (0,0,4,40,.15),
@@ -44,7 +45,7 @@ for bdr_g, p_g, bdr_h, p_h, vac in scenarios:
 
     data = pd.DataFrame([col1, col2, values]).T
     data.columns = ['Type1', 'Type2', 'Values']
-    print(data)
+    #print(data)
 
     entry.extend(data.groupby('Type1').sum().Values.values)
     entry.append(np.sum(entry[-1] - entry[-2]))
@@ -53,7 +54,20 @@ columns = ['bdr_g', 'p_g', 'bdr_h', 'p_h',
            'vac', 'expenses', 'revenue','income'] 
 pd.DataFrame(scenes, columns = columns).to_csv('scenarios.csv', index=False)
 data.to_csv('house.csv', index=False)
-print(data.groupby('Type1').sum())
+#print(data.groupby('Type1').sum())
+monthly_cashflow = [ 12*x[-1] for x in scenes]
+names = ['Status Quo', 'Light Rehab', 'Low Rehab', 'Medium Rehab', 
+         'High Rehab']
+
+fig, ax = plt.subplots()
+#fig.figsize=(15,15)
+ax.bar(names, monthly_cashflow, .35)
+ax.set_ylabel('$')
+ax.set_title('Yearly Cash Flow')
+plt.xticks(rotation=45)
+plt.savefig('yearly_cash_flow', dpi=150)
+fig.tight_layout()
+plt.show()
 ## monthly cash flow
 #total_monthly_cashflow = total_monthly_income_current - total_monthly_expenses
 #
